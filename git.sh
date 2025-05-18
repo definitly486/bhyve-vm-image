8981bbbea6e99cd18e6230c19fa5477c46ae94a0
Your Name <you@example.com>
2025-05-18 10:29:40 +0300

    новый файл:    android-blissos/blissos.sh 	новый файл:    git.sh

---
+++ git.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+dir=$(dirname "$(realpath $0)")
+cd $dir
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+git add --all
+git add .
+git commit -n
+git push https://github.com/definitly486/bhyve-vm-image.git
+
+
