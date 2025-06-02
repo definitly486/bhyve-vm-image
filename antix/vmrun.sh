212503026bf6724c5584836ce1cc315a62879be2
Your Name <you@example.com>
2025-06-02 20:46:51 +0300

    переименовано: antix/antix.sh -> antix/vmrun.sh

--- antix/vmrun.sh
+++ antix/vmrun.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
 name=antix
 dir=$(dirname "$(realpath $0)")
-CD=/home/definitly/Downloads/antiX-23.2_x64-base.iso
+
+
+if [ -z "$1" ]
+then
+     echo "запуск без CD"
+     CD=
+else
+     CD="-s 2,ahci-cd,/ntfs-2TB/vm/ISO/$1"
+    echo $CD
+fi
+
 HD=$dir/$name.img
 USB="5/0/0"
 #UEFI=/usr/local/share/uefi-firmware/BHYVE_UEFI.fd
 UEFI=$dir/BHYVE_BHF_UEFI.fd
-MEM=1G
+MEM=2G
 VM=$name
 IF="tap0"
 MAC="mac=00:A0:98:78:32:10"
 DPY="w=1918,h=1058"
 #-s 2,ahci-cd,$CD \
+#truncate -s 15GB mageia.img
 doas  ifconfig $IF up
 while true
 do
@@ -19,9 +30,11 @@ do
       -c 1 \
       -s 0,hostbridge \
       -s 3,ahci-hd,$HD,sectorsize=512 \
+ $CD  \
 -s 5,fbuf,tcp=0.0.0.0:5900,$DPY \
       -s 6,xhci,tablet \
       -s 10,virtio-net,$IF \
+     -s 15,virtio-9p,sharename=/home/ \
       -s 20,hda,play=/dev/dsp,rec=/dev/dsp \
       -s 31,lpc \
       -l bootrom,$UEFI \
